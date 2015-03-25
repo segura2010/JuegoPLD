7362c5a5ca7c170117beda5c2bcf951e1c9bb68f
alberto__segura <alberto.segura.delgado@gmail.com>
2015-03-25 19:09:41 +0100

    arreglando cosas con las nuevas fisicas

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ var mainState = {
         game.stage.backgroundColor = '#04B404';
 
         // Load images
-        game.load.image('principalPlayer', 'assets/player.png');  
+        game.load.image('principalPlayer', 'assets/granada.png');  
         game.load.image('otherPlayers', 'assets/pipe.png'); 
         game.load.image('porteria', 'assets/pipe.png');
-        game.load.image('ball', 'assets/pipe.png'); 
+        game.load.image('ball', 'assets/player.png'); 
 
         // Load sounds
         game.load.audio('goal', 'assets/jump.wav');
@@ -32,15 +32,17 @@ var mainState = {
         //this.principalPlayer.body.mass = 0.01;
         //this.principalPlayer.body.kinematic = true;
         this.principalPlayer.body.collideWorldBounds = true;
+        this.principalPlayer.body.name = "principalPlayer";
 
         this.ball = game.add.sprite(100, 100, "ball");
         game.physics.p2.enable(this.ball);
-        this.ball.scale.x = 0.2; this.ball.scale.y = 0.2;
-        this.ball.body.setCircle( 28 /*this.ball.width * 0.95*/);
+        this.ball.scale.x = 0.13; this.ball.scale.y = 0.13;
+        this.ball.body.setCircle( 20 /*this.ball.width * 0.95*/);
         this.ball.body.debug = false;
-        this.ball.body.mass = 1;
+        //this.ball.body.mass = 1;
         this.ball.body.kinematic = false;
         this.ball.body.collideWorldBounds = true;
+        this.ball.body.name = "principalPlayer";
 
         // Prepare key events!
         this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
@@ -54,20 +56,22 @@ var mainState = {
 
         // Porterias creation
         this.localPorteria = this.game.add.sprite(-45, GAMESIZE[1]/2, 'porteria');
+        game.physics.p2.enable(this.localPorteria);
+        this.localPorteria.body.immovable = true;
         this.localPorteria.height = 100;
         this.upperLocalLimit = this.createCircle(0, (GAMESIZE[1]/2)+(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
         this.upperLocalLimit.body.immovable = true;
         this.lowerLocalLimit = this.createCircle(0, (GAMESIZE[1]/2)-(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
         this.lowerLocalLimit.body.immovable = true;
-        //game.physics.p2.enable(this.localPorteria);
 
         this.visitantPorteria = this.game.add.sprite(GAMESIZE[0]-5, GAMESIZE[1]/2, 'porteria');
+        game.physics.p2.enable(this.visitantPorteria);
+        this.visitantPorteria.body.immovable = true;
         this.visitantPorteria.height = 100;
         this.upperVisitantLimit = this.createCircle(GAMESIZE[0]-4, (GAMESIZE[1]/2)+(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
         this.upperVisitantLimit.body.immovable = true;
         this.lowerVisitantLimit = this.createCircle(GAMESIZE[0]-4, (GAMESIZE[1]/2)-(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
         this.lowerVisitantLimit.body.immovable = true;
-        //game.physics.p2.enable(this.visitantPorteria);
         
 
  
@@ -76,12 +80,6 @@ var mainState = {
         this.downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
         this.rigthKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
         this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        /*
-        this.upKey.onDown.add(this.goUp, this);
-        this.downKey.onDown.add(this.goDown, this);
-        this.rigthKey.onDown.add(this.goRigth, this);
-        this.leftKey.onDown.add(this.goLeft, this);
-        */
 
         this.visitantScore = 0;
         this.localScore = 0;
@@ -91,11 +89,14 @@ var mainState = {
         //this.jumpSound = this.game.add.audio('jump');
 
         // Overlap con P2JS
-        //game.physics.p2.setPostBroadphaseCallback(checkOverlap, this); 
+        game.physics.p2.setPostBroadphaseCallback(this.checkOverlap, this); 
     },
 
     update: function() {
         this.labelScore.text = this.localScore+"-"+this.visitantScore;
+
+        this.localPorteria.body.setZeroVelocity();
+        this.visitantPorteria.body.setZeroVelocity();
 
         this.addAcceleration();
 
@@ -215,14 +216,18 @@ var mainState = {
     },
     addAcceleration: function() {
         // http://jsfiddle.net/sh036s95/
-        var MAXVEL = 100; 
+        // http://www.html5gamedevs.com/topic/7474-set-constant-velocity-for-body-physicsp2js/
+        var MAXVEL = 270;
         /*
         this.vy = this.principalPlayer.body.velocity.y;
         this.vx = this.principalPlayer.body.velocity.x;
         */
+
+
+               
         if(this.upKey.isDown && this.vy > -MAXVEL)
         {   this.vy += -10;
-            this.principalPlayer.body.velocity.y = this.vy;
+            this.principalPlayer.body.velocity.y = 10;
         }
         else if(this.downKey.isDown && this.vy < MAXVEL)
         {   this.vy += 10;
@@ -236,24 +241,27 @@ var mainState = {
         {   this.vy += 10;
             this.principalPlayer.body.velocity.y = this.vy;
         }
+        
 
         if(this.leftKey.isDown && this.vx > -MAXVEL)
         {   this.vx += -10;
-            this.principalPlayer.body.velocity.x += this.vx;
+            this.principalPlayer.body.velocity.x = this.vx;
         }
         else if(this.rigthKey.isDown && this.vx < MAXVEL)
         {   this.vx += 10;
-            this.principalPlayer.body.velocity.x += this.vx;
+            this.principalPlayer.body.velocity.x = this.vx;
         }
         else if(this.vx > 0)
         {   this.vx += -10;
-            this.principalPlayer.body.velocity.x += this.vx;
+            this.principalPlayer.body.velocity.x = this.vx;
         }
         else if(this.vx < 0)
         {   this.vx += 10;
-            this.principalPlayer.body.velocity.x += this.vx;
+            this.principalPlayer.body.velocity.x = this.vx;
         }
-        console.log(this.vx+":"+this.vy)
+        
+        
+        //console.log(this.vx+":"+this.vy)
     },
     kickBall: function(ball, player) {
         this.ball.body.velocity = player.body.velocity;
@@ -291,6 +299,12 @@ var mainState = {
     visitantGoal: function() {
         this.visitantScore += 1;
         this.ball.reset(GAMESIZE[0]/2, GAMESIZE[1]/2);
+    },
+    checkOverlap: function(body1, body2){
+        console.log(body1.name);
+        //if( body1.sprite.name == body2.sprite.name )
+
+        return true;
     }
 };
 
