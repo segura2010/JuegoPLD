91a7e2e35a80763715f4c2cb3a6fab388808af33
alberto__segura <alberto.segura.delgado@gmail.com>
2015-03-26 12:47:13 +0100

    Funciones para creacion de jugadores, porterias y balon. Los jugadores se meten en un Map

--- main.js
+++ main.js
@@ -23,26 +23,7 @@ var mainState = {
         
         game.physics.startSystem(Phaser.Physics.P2JS);
         game.physics.p2.restitution = 0.5;
-
-        this.principalPlayer = game.add.sprite(200, 200, "principalPlayer");
-        game.physics.p2.enable(this.principalPlayer);
-        this.principalPlayer.scale.x = 0.2; this.principalPlayer.scale.y = 0.2;
-        this.principalPlayer.body.setCircle( 28 /*this.principalPlayer.width * 0.95*/);
-        //this.principalPlayer.body.debug = false;
-        //this.principalPlayer.body.mass = 0.01;
-        //this.principalPlayer.body.kinematic = true;
-        this.principalPlayer.body.collideWorldBounds = true;
-        this.principalPlayer.body.name = "principalPlayer";
-
-        this.ball = game.add.sprite(100, 100, "ball");
-        game.physics.p2.enable(this.ball);
-        this.ball.scale.x = 0.13; this.ball.scale.y = 0.13;
-        this.ball.body.setCircle( 20 /*this.ball.width * 0.95*/);
-        this.ball.body.debug = false;
-        //this.ball.body.mass = 1;
-        this.ball.body.kinematic = false;
-        this.ball.body.collideWorldBounds = true;
-        this.ball.body.name = "principalPlayer";
+        game.physics.p2.gravity.y = 0;
 
         // Prepare key events!
         this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
@@ -50,36 +31,13 @@ var mainState = {
         this.rigthKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
         this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
 
-        // Principal Player velocity
-        this.vx = 0;
-        this.vy = 0;
-
-        // Porterias creation
-        this.localPorteria = this.game.add.sprite(-45, GAMESIZE[1]/2, 'porteria');
-        game.physics.p2.enable(this.localPorteria);
-        this.localPorteria.body.immovable = true;
-        this.localPorteria.height = 100;
-        this.upperLocalLimit = this.createCircle(0, (GAMESIZE[1]/2)+(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
-        this.upperLocalLimit.body.immovable = true;
-        this.lowerLocalLimit = this.createCircle(0, (GAMESIZE[1]/2)-(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
-        this.lowerLocalLimit.body.immovable = true;
-
-        this.visitantPorteria = this.game.add.sprite(GAMESIZE[0]-5, GAMESIZE[1]/2, 'porteria');
-        game.physics.p2.enable(this.visitantPorteria);
-        this.visitantPorteria.body.immovable = true;
-        this.visitantPorteria.height = 100;
-        this.upperVisitantLimit = this.createCircle(GAMESIZE[0]-4, (GAMESIZE[1]/2)+(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
-        this.upperVisitantLimit.body.immovable = true;
-        this.lowerVisitantLimit = this.createCircle(GAMESIZE[0]-4, (GAMESIZE[1]/2)-(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
-        this.lowerVisitantLimit.body.immovable = true;
+        this.players = {};
         
+        this.createPorterias();
 
- 
-        // Prepare key events!
-        this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
-        this.downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-        this.rigthKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-        this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        this.createPlayer("principalPlayer", "principalPlayer");
+
+        this.createBall("ball");        
 
         this.visitantScore = 0;
         this.localScore = 0;
@@ -90,9 +48,11 @@ var mainState = {
 
         // Overlap con P2JS
         game.physics.p2.setPostBroadphaseCallback(this.checkOverlap, this); 
+
     },
 
     update: function() {
+
         this.labelScore.text = this.localScore+"-"+this.visitantScore;
 
         this.localPorteria.body.setZeroVelocity();
@@ -100,9 +60,10 @@ var mainState = {
 
         this.addAcceleration();
 
-        
+        /*
         if (this.principalPlayer.inWorld == false)
             this.restartGame();
+        */
     
 
         // If player and ball overlap, is a kick!!
@@ -124,144 +85,59 @@ var mainState = {
             this.bird.angle += 1;  
             */ 
     },
-
-    jump: function() {
-        // If the bird is dead, he can't jump
-        if (this.bird.alive == false)
-            return; 
-
-        this.bird.body.velocity.y = -350;
-
-        // Jump animation
-        game.add.tween(this.bird).to({angle: -20}, 100).start();
-
-        // Play sound
-        this.jumpSound.play();
-    },
-
-    hitPipe: function() {
-        // If the bird has already hit a pipe, we have nothing to do
-        if (this.bird.alive == false)
-            return;
-            
-        // Set the alive property of the bird to false
-        this.bird.alive = false;
-
-        // Prevent new pipes from appearing
-        this.game.time.events.remove(this.timer);
-    
-        // Go through all the pipes, and stop their movement
-        this.pipes.forEachAlive(function(p){
-            p.body.velocity.x = 0;
-        }, this);
-    },
-
     restartGame: function() {
         game.state.start('main');
-    },
-
-    addOnePipe: function(x, y) {
-        var pipe = this.pipes.getFirstDead();
-
-        pipe.reset(x, y);
-        pipe.body.velocity.x = -200;  
-        pipe.checkWorldBounds = true;
-        pipe.outOfBoundsKill = true;
-    },
-
-    addRowOfPipes: function() {
-        var hole = Math.floor(Math.random()*5)+1;
-        
-        for (var i = 0; i < 8; i++)
-            if (i != hole && i != hole +1) 
-                this.addOnePipe(400, i*60+10);   
-    
-        this.score += 1;
-        this.labelScore.text = this.score;  
-    },
-
-
-    goUp: function() {
-        this.principalPlayer.body.velocity.y = -10;
-    },
-    goDown: function() {
-        this.principalPlayer.body.velocity.y = 10;
-    },
-    goRigth: function() {
-        this.principalPlayer.body.velocity.x = -10;
-    },
-    goLeft: function() {
-        this.principalPlayer.body.velocity.x = 10;
-    },
-    stopPlayer: function(player)
-    {   // Stops player
-        player.body.velocity.y = 0;
-        player.body.velocity.y = 0;
-        player.body.velocity.x = 0;
-        player.body.velocity.x = 0;
-    },
-    stopBall: function()
-    {   // Stops player
-        this.ball.body.velocity.y = 0;
-        this.ball.body.velocity.y = 0;
-        this.ball.body.velocity.x = 0;
-        this.ball.body.velocity.x = 0;
-    },
-    same: function()
-    {   // Stops player
-        this.ball.body.velocity.y = 0;
-        this.ball.body.velocity.y = 0;
-        this.ball.body.velocity.x = 0;
-        this.ball.body.velocity.x = 0;
     },
     addAcceleration: function() {
         // http://jsfiddle.net/sh036s95/
         // http://www.html5gamedevs.com/topic/7474-set-constant-velocity-for-body-physicsp2js/
         var MAXVEL = 270;
         /*
-        this.vy = this.principalPlayer.body.velocity.y;
-        this.vx = this.principalPlayer.body.velocity.x;
+        this.players[p].vy = this.principalPlayer.body.velocity.y;
+        this.players[p].vx = this.principalPlayer.body.velocity.x;
         */
+        
+        //var gameCopy = this;
+        for(p in this.players)
+        { 
+            if(this.upKey.isDown && this.players[p].vy > -MAXVEL)
+            {   this.players[p].vy += -10;
+                this.players[p].body.velocity.y = 10;
+            }
+            else if(this.downKey.isDown && this.players[p].vy < MAXVEL)
+            {   this.players[p].vy += 10;
+                this.players[p].body.velocity.y = this.players[p].vy;
+            }
+            else if(this.players[p].vy > 0)
+            {   this.players[p].vy += -10;
+                this.players[p].body.velocity.y = this.players[p].vy;
+            }
+            else if(this.players[p].vy < 0)
+            {   this.players[p].vy += 10;
+                this.players[p].body.velocity.y = this.players[p].vy;
+            }
+            
 
-
-               
-        if(this.upKey.isDown && this.vy > -MAXVEL)
-        {   this.vy += -10;
-            this.principalPlayer.body.velocity.y = 10;
-        }
-        else if(this.downKey.isDown && this.vy < MAXVEL)
-        {   this.vy += 10;
-            this.principalPlayer.body.velocity.y = this.vy;
-        }
-        else if(this.vy > 0)
-        {   this.vy += -10;
-            this.principalPlayer.body.velocity.y = this.vy;
-        }
-        else if(this.vy < 0)
-        {   this.vy += 10;
-            this.principalPlayer.body.velocity.y = this.vy;
+            if(this.leftKey.isDown && this.players[p].vx > -MAXVEL)
+            {   this.players[p].vx += -10;
+                this.players[p].body.velocity.x = this.players[p].vx;
+            }
+            else if(this.rigthKey.isDown && this.players[p].vx < MAXVEL)
+            {   this.players[p].vx += 10;
+                this.players[p].body.velocity.x = this.players[p].vx;
+            }
+            else if(this.players[p].vx > 0)
+            {   this.players[p].vx += -10;
+                this.players[p].body.velocity.x = this.players[p].vx;
+            }
+            else if(this.players[p].vx < 0)
+            {   this.players[p].vx += 10;
+                this.players[p].body.velocity.x = this.players[p].vx;
+            }
         }
         
-
-        if(this.leftKey.isDown && this.vx > -MAXVEL)
-        {   this.vx += -10;
-            this.principalPlayer.body.velocity.x = this.vx;
-        }
-        else if(this.rigthKey.isDown && this.vx < MAXVEL)
-        {   this.vx += 10;
-            this.principalPlayer.body.velocity.x = this.vx;
-        }
-        else if(this.vx > 0)
-        {   this.vx += -10;
-            this.principalPlayer.body.velocity.x = this.vx;
-        }
-        else if(this.vx < 0)
-        {   this.vx += 10;
-            this.principalPlayer.body.velocity.x = this.vx;
-        }
         
-        
-        //console.log(this.vx+":"+this.vy)
+        //console.log(this.players[p].vx+":"+this.players[p].vy)
     },
     kickBall: function(ball, player) {
         this.ball.body.velocity = player.body.velocity;
@@ -295,18 +171,79 @@ var mainState = {
     localGoal: function() {
         this.localScore += 1;
         this.ball.reset(GAMESIZE[0]/2, GAMESIZE[1]/2);
+        return true;
     },
     visitantGoal: function() {
         this.visitantScore += 1;
         this.ball.reset(GAMESIZE[0]/2, GAMESIZE[1]/2);
+        return true;
     },
     checkOverlap: function(body1, body2){
-        console.log(body1.name);
-        //if( body1.sprite.name == body2.sprite.name )
+        // console.log(body1.name+":"+body2.name);
+        if( ((body1.name == this.localPorteria.body.name) && (body2.name == this.ball.body.name)) || ((body1.name == this.ball.body.name) && (body2.name == this.localPorteria.body.name)) )
+        {
+            this.localGoal();
+        }
+        if( ((body1.name == this.visitantPorteria.body.name) && (body2.name == this.ball.body.name)) || ((body1.name == this.ball.body.name) && (body2.name == this.visitantPorteria.body.name)) )
+        {
+            this.visitantGoal();
+        }
 
         return true;
+    },
+    createPorterias: function()
+    {
+        // Porterias creation
+        this.localPorteria = this.game.add.sprite(0, GAMESIZE[1]/2, 'porteria');
+        game.physics.p2.enable(this.localPorteria);
+        this.localPorteria.body.immovable = true;
+        this.localPorteria.body.collideWorldBounds = false;
+        this.localPorteria.body.name = "localPorteria";
+        this.localPorteria.height = 100;
+        this.upperLocalLimit = this.createCircle(0, (GAMESIZE[1]/2)+(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
+        this.upperLocalLimit.body.immovable = true;
+        this.lowerLocalLimit = this.createCircle(0, (GAMESIZE[1]/2)-(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
+        this.lowerLocalLimit.body.immovable = true;
+
+        this.visitantPorteria = this.game.add.sprite(GAMESIZE[0], GAMESIZE[1]/2, 'porteria');
+        game.physics.p2.enable(this.visitantPorteria);
+        this.visitantPorteria.body.immovable = true;
+        this.visitantPorteria.body.collideWorldBounds = false;
+        this.visitantPorteria.body.name = "visitantPorteria";
+        this.visitantPorteria.height = 100;
+        this.upperVisitantLimit = this.createCircle(GAMESIZE[0]-4, (GAMESIZE[1]/2)+(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
+        this.upperVisitantLimit.body.immovable = true;
+        this.lowerVisitantLimit = this.createCircle(GAMESIZE[0]-4, (GAMESIZE[1]/2)-(this.localPorteria.height/2)-5, 7, 0xFAFAFA);
+        this.lowerVisitantLimit.body.immovable = true;
+    },
+    createPlayer: function(sprite, name)
+    {
+        this.players[name] = game.add.sprite(200, 200, sprite);
+        game.physics.p2.enable(this.players[name]);
+        this.players[name].scale.x = 0.2; this.players[name].scale.y = 0.2;
+        this.players[name].body.setCircle( 28 /*this.principalPlayer.width * 0.95*/);
+        //this.principalPlayer.body.debug = false;
+        //this.principalPlayer.body.mass = 0.01;
+        //this.principalPlayer.body.kinematic = true;
+        this.players[name].body.collideWorldBounds = true;
+        this.players[name].body.name = name;
+        this.players[name].vx = 0;
+        this.players[name].vy = 0;
+    },
+    createBall: function(sprite)
+    {
+        this.ball = game.add.sprite(100, 100, sprite);
+        game.physics.p2.enable(this.ball);
+        this.ball.scale.x = 0.13; this.ball.scale.y = 0.13;
+        this.ball.body.setCircle( 20 /*this.ball.width * 0.95*/);
+        this.ball.body.debug = false;
+        //this.ball.body.mass = 1;
+        this.ball.body.kinematic = false;
+        this.ball.body.collideWorldBounds = true;
+        this.ball.body.name = "ball";
     }
 };
+
 
 game.state.add('main', mainState);  
 game.state.start('main'); 
