d76b69f34bb3458f94c015361a2af59916c3aabf
EvaAlmansa <evaalmansa555@hotmail.com>
2015-04-14 09:13:59 +0200

    Visualización del menú
    Sólo se ve lo que es la parte de inicio del menú. Falta la funcionalidad
    de los botones.

--- menu.js
+++ menu.js
@@ -1,8 +1,8 @@
 // Menu Game State
 var menuState = {
     preload: function() {
-        game.stage.backgroundColor = 'grey';
-
+        //game.stage.backgroundColor = 'grey';
+     
         // Load images
         game.load.image('visitantPlayer', 'assets/visitant.png'); 
         game.load.image('localPlayer', 'assets/local.png'); 
@@ -10,13 +10,27 @@ var menuState = {
         game.load.image('ball', 'assets/ball.png'); 
         game.load.image('field', 'assets/campo.jpeg'); 
 
+        //Load images menu
+        game.load.image('back_field', 'assets/fondo_menu.png');
+        game.load.image('boton1Player', 'assets/1jugador.png');
+        game.load.image('boton2Players', 'assets/2jugadores.png');
+
         // Load sounds
         game.load.audio('goalAudio', 'assets/goal.mp3');
         game.load.audio('collisionAudio', 'assets/collision.mp3'); 
     },
 
     create: function() { 
-        this.startIABtn = this.game.add.button(0, 0, "ball", startOnePlayerGame, this); 
+        //this.startIABtn = this.game.add.button(0, 0, "ball", startOnePlayerGame, this); 
+
+        this.back_field = game.add.image(0, 0, 'back_field');
+        this.back_field.scale.x = 1.4; this.back_field.scale.y = 1.28;
+
+        this.startOnePlayerGame = this.game.add.button(350, 200, 'boton1Player');
+        //this.startOnePlayerGame = 1; this.boton1Player.scale.y = 0.9;
+
+        this.startTwoPlayersGame = game.add.button(350, 300, 'boton2Players');
+        //this.startTwoPlayersGame = 0.508; this.boton2Players.scale.y = 0.46;
     },
 
     update: function() {
