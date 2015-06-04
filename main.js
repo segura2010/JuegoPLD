15defe2777b0ccec46735a52511da7b6a8844184
EvaAlmansa <evaalmansa555@hotmail.com>
2015-06-04 13:27:11 +0200

    Muestra el resultado de quién gana centrado

--- main.js
+++ main.js
@@ -392,13 +392,13 @@ var mainState = {
         if(goalLimit <= this.localScore)
         {
             this.endGame();
-            this.alertMessages.text = "LOCAL ha GANADO!";
+            this.alertMessages.text = "LOCAL \nha GANADO!";
             setTimeout(mostrarMenu, 2500);
         }
         else if(goalLimit <= this.visitantScore)
         {
             this.endGame();
-            this.alertMessages.text = "VISITANTE ha GANADO!";
+            this.alertMessages.text = "VISITANTE \nha GANADO!";
             setTimeout(mostrarMenu, 2500);
         }
         else
